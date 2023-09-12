cfde030724180166e7e87a10b98a1f20616aba24
ishika2203 <ishikabatra.net@gmail.com>
2023-09-12 14:09:45 +0530

    global object added

---
+++ nodejs-basics/global.js
@@ -0,0 +1,16 @@
+// GLOBAL OBJECT
+
+// console.log(global);
+
+// setTimeout(() => {
+//   console.log('Hi Everyone!');
+//   clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//   console.log('hi');
+// }, 1000);
+
+// console.log(__dirname); //return the absolute path of the folder, in which this file is
+
+// console.log(__filename); //return the absolute path of the file, on which we are working on
