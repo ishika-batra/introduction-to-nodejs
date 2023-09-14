d1893947c5bb84e5c2c702abbf546581a350fb55
ishika2203 <ishikabatra.net@gmail.com>
2023-09-14 15:07:31 +0530

    streams and buffers added

---
+++ nodejs-basics/streams.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./docs/blog2.txt', {
+  encoding: 'utf8',
+});
+
+const writeStream = fs.createWriteStream('./docs/blog3.txt');
+
+// readStream.on('data', (chunk) => {
+//   console.log('--NEW CHUNK--');
+//   console.log(chunk);
+//   writeStream.write('\nNEW CHUNK\n');
+//   writeStream.write(chunk);
+// });
+
+// SHORTCUT FOR ABOVE
+// PIPING
+readStream.pipe(writeStream);
