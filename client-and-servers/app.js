f6f56df54ba41e1741f39864bc378d862e7eb090
ishika2203 <ishikabatra.net@gmail.com>
2023-09-25 10:57:23 +0530

    middleware added

--- client-and-servers/app.js
+++ client-and-servers/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const morgan = require('morgan');
 // express app
 const app = express();
 
@@ -8,6 +8,25 @@ app.set('view engine', 'ejs');
 
 // listen for requests
 app.listen(3000);
+
+// MIDDLEWARE AND STATIC FILES
+app.use(express.static('public'));
+
+// THIRD PARTY MIDDLEWARE
+app.use(morgan('dev'));
+
+// app.use((req, res, next) => {
+//   console.log('new request made:');
+//   console.log('host: ', req.hostname);
+//   console.log('path: ', req.path);
+//   console.log('method: ', req.method);
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log('in the next middleware');
+//   next();
+// });
 
 app.get('/', (req, res) => {
   // res.send('<p> HOME PAGE </p>');
