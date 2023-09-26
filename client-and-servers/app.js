b83dd4082b36fde07422d9067bc2dc55258372b5
ishika2203 <ishikabatra.net@gmail.com>
2023-09-26 18:01:47 +0530

    post request added

--- client-and-servers/app.js
+++ client-and-servers/app.js
@@ -18,6 +18,7 @@ app.set('view engine', 'ejs');
 
 // MIDDLEWARE AND STATIC FILES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 // THIRD PARTY MIDDLEWARE
 app.use(morgan('dev'));
@@ -104,6 +105,18 @@ app.get('/blogs', (req, res) => {
     .sort({ createdAt: -1 })
     .then((result) => {
       res.render('index', { title: 'All Blogs', blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then((result) => {
+      res.redirect('/blogs');
     })
     .catch((err) => {
       console.log(err);
